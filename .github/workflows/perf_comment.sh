c54f43378fffc6e92741515c43583aadf9efd113
sanix-darker <s4nixd@gmail.com>
2022-05-15 20:49:37 +0200

    ci: updates12

--- .github/workflows/perf_comment.sh
+++ .github/workflows/perf_comment.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 _main(){
-    echo "### On Master"
+    echo "## Performances report"
+    echo "Hi there, this is some performances reports for changes you made !"
+    echo "#### On $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@') branch"
     cat ./.benchmarks/*/0001_bench.json | jq -r '["| func     ", "| min       ", "| max       ", "| mean       ", "| median       ", "| total      ", "| iterations       ", "| full_path       |"], ["| -------", "| -------", "| -------", "| -------", "| -------", "| -------","| -------", "| ------- |"], (.benchmarks[] | ["| " + .name, "| " + (.stats.min|tostring)[0:10], "| " + (.stats.max|tostring)[0:10], "| " + (.stats.mean|tostring)[0:10], "| " + (.stats.median|tostring)[0:10], "| " + (.stats.total|tostring)[0:10], "| " + (.stats.iterations|tostring)[0:10], "| " + (.fullname[6:-1]) + "|"]) | @tsv'
-    echo "### On $1"
+    echo "#### On $1 branch"
     cat ./.benchmarks/*/0002_bench.json | jq -r '["| func     ", "| min       ", "| max       ", "| mean       ", "| median       ", "| total      ", "| iterations       ", "| full_path       |"], ["| -------", "| -------", "| -------", "| -------", "| -------", "| -------","| -------", "| ------- |"], (.benchmarks[] | ["| " + .name, "| " + (.stats.min|tostring)[0:10], "| " + (.stats.max|tostring)[0:10], "| " + (.stats.mean|tostring)[0:10], "| " + (.stats.median|tostring)[0:10], "| " + (.stats.total|tostring)[0:10], "| " + (.stats.iterations|tostring)[0:10], "| " + (.fullname[6:-1]) + "|"]) | @tsv'
 }
 
